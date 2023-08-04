2c01979cbf87f876fa96d21de137d50067614815
alyssakamara <aakamara@psehealthyenergy.org>
2023-08-04 09:33:40 -0400

    Update Bivariate.R

--- Bivariate.R
+++ Bivariate.R
@@ -14,20 +14,34 @@ library(janitor)
 #Census tract
 census_tracts <- st_read("C:\\Users\\AlyssaKamara\\Desktop\\nhgis0004_shape\\nhgis0004_shape\\nhgis0004_shapefile_tl2010_us_tract_2010\\US_tract_2010.shp") %>%
   clean_names() %>%
-  dplyr::filter(statefp10 == 06) %>%
-  dplyr::filter(countyfp10 == 013) %>%
-  dplyr::select(geoid10,gisjoin) #when I run this portion the data turns up having 0 observations of 3 variables?
+  dplyr::filter(statefp10 == "06") %>%
+  dplyr::filter(countyfp10 == "013") %>%
+  dplyr::select(geoid10,gisjoin)
+
+class(census_tracts)
+
 
 #Vulnerability data
-vunerability_data <-read.csv("C:\\Users\\AlyssaKamara\\Desktop\\full_indicators_pctl_domain_scores_climate.csv")%>%
-  mutate(gisjoin = str_pad(gisjoin, 12, side = "left", pad = "0")) #how to fix for this problem
+vunerability_data <-read.csv("C:\\Users\\AlyssaKamara\\Desktop\\full_indicators_pctl_domain_scores_climate.csv") %>%
+  dplyr::select(gisjoin, state_name,county,tract,pop_2015_2019_ces,fips,state,county_name,extrm_heat_2030_2050,pctl_avg_energy_burden_percent_income,pm2_5_ces)%>%
+  gisjoin = as.character(gisjoin)
+
+  colnames(vunerability_data)
+  class(vunerability_data)
   
 #join datasets
 crs_wgs84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
-vunerability_sp <- left_join(vunerability_data, census_tracts)
+vunerability_sp <- right_join(vunerability_data, census_tracts) 
   st_as_sf() %>%
     st_transform(crs_wgs84)
+
+  class(vunerability_sp)
+
+  common_column <- "common_column_name"
+  print(common_column %in% colnames(vunerability_data))
+  print(common_column %in% colnames(census_tracts))
+  
   
 #interactive map
   # create a continuous pallette function 
@@ -41,7 +55,7 @@ vunerability_sp <- left_join(vunerability_data, census_tracts)
   leaflet(data = vunerability_sp) %>%
     addTiles() %>%
     setView(lng = -118, lat = 37, zoom = 6) %>%
-    addPolygons(color = "#444444", # lines color
+    addPolygons(color = "#444444", # lines color #add markers when I do it again for air monitors
                 weight = 1, # lines thickness
                 smoothFactor = 0.5, #lines smoothness
                 opacity = 1.0, 
@@ -54,4 +68,9 @@ vunerability_sp <- left_join(vunerability_data, census_tracts)
               # opacity = 0.9, 
               # title = "Distance", 
               position = "bottomleft")
+  
+  #do both maps in this script to avoid having to clean data
+  # next map: PM 2.5 and monitors
+  
+  #load monitor maps data
   
